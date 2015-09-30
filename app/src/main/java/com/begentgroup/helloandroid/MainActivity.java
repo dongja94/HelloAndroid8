6ad7a5cd905a673afcf2fb7d8cf026a37bb4334b
dongja94 <dongja94@gmail.com>
2015-09-30 17:30:04 +0900

    id

--- app/src/main/java/com/begentgroup/helloandroid/MainActivity.java
+++ app/src/main/java/com/begentgroup/helloandroid/MainActivity.java
@@ -1,16 +1,32 @@
 package com.begentgroup.helloandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView messageView;
+    EditText inputView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        messageView = (TextView)findViewById(R.id.text_message);
+        inputView = (EditText)findViewById(R.id.edit_input);
+        Button btn = (Button)findViewById(R.id.btn_send);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageView.setText(inputView.getText().toString());
+            }
+        });
     }
 
     @Override
