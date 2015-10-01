b99e918ec069c398203cf54331c4a3e470b1e328
dongja94 <dongja94@gmail.com>
2015-10-01 09:58:31 +0900

    action

--- app/src/main/java/com/begentgroup/helloandroid/MainActivity.java
+++ app/src/main/java/com/begentgroup/helloandroid/MainActivity.java
@@ -1,5 +1,7 @@
 package com.begentgroup.helloandroid;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -25,6 +27,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 messageView.setText(inputView.getText().toString());
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_google);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com"));
+                startActivity(intent);
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_dial);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:010-2257-3585"));
+                startActivity(intent);
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_other);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                startActivity(intent);
             }
         });
     }
